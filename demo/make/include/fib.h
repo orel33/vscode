9780222f3abca436fa2f972b37863bc9fb041798
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2018-11-22 17:56:46 +0100

    add fib C demo with Makefile

---
+++ demo/make/include/fib.h
@@ -0,0 +1,13 @@
+#ifndef FIB_H
+#define FIB_H
+
+/**
+ * @brief compute Fibonacci sequence
+ *
+ * @param n sequence length
+ * @details https://en.wikipedia.org/wiki/Fibonacci_number
+ * @return the n'th term of Fibonacci sequence 
+ */
+int fib(int n);
+
+#endif
