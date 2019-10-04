7a2ccb1f0ce00037199a65148a2e3a1fafe4fd5b
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2019-10-04 18:24:49 +0200

    update clean.sh

--- clean.sh
+++ clean.sh
@@ -2,14 +2,15 @@
 
 # status
 echo "Your VS Code settings and cache files (before):"
-du -sh ~/.vscode ~/.config/Code/
+du -sh ~/.vscode/ ~/.config/Code/ ~/.vscode-cpptools/
 echo
 echo "Cleaning cache files..."
 rm -rf ~/.config/Code/Cache/*
 rm -rf ~/.config/Code/CacheData/*
 rm -rf ~/.config/Code/User/workspaceStorage/*
+rm -rf ~/.vscode-cpptools/ipch/*
 echo
 echo "Your VS Code settings and cache files (after):"
-du -sh ~/.vscode ~/.config/Code/
+du -sh ~/.vscode/ ~/.config/Code/ ~/.vscode-cpptools/
 echo
 echo "Done!"
