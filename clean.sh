bc5d02a0852ce69a3d57bb0efde31adddf268c5f
orel <aurelien.esnard@u-bordeaux.fr>
2021-09-21 14:38:13 +0200

    fix clean cache

--- clean.sh
+++ clean.sh
@@ -2,15 +2,17 @@
 
 # status
 echo "Your VS Code settings and cache files (before):"
-du -sch ~/.vscode/ ~/.config/Code/ ~/.vscode-cpptools/
+du -sch ~/.vscode/ ~/.config/Code/
 echo
 echo "Cleaning cache files..."
-rm -rf ~/.config/Code/Cache/*
+rm -rf ~/.config/Code/Cache/* 
 rm -rf ~/.config/Code/CacheData/*
 rm -rf ~/.config/Code/User/workspaceStorage/*
-rm -rf ~/.vscode-cpptools/ipch/*
+rm -rf ~/.config/Code/CachedExtensionVSIXs/*
+
 echo
 echo "Your VS Code settings and cache files (after):"
-du -sch ~/.vscode/ ~/.config/Code/ ~/.vscode-cpptools/
+du -sch ~/.vscode/ ~/.config/Code/ 
+
 echo
 echo "Done!"
