9a0e70138a65d1222d9cc9f9c4e39e31a5d130a7
Aurelien Esnard <aurelien.esnard@u-bordeaux.fr>
2021-09-24 09:29:41 +0200

    minor fix

--- migrate.sh
+++ migrate.sh
@@ -9,10 +9,11 @@ function moveit() {
     local DST="$XWORK/$WHAT"
     [ -L $SRC ] && echo "Warning: link \"$SRC\" already exists! Skip..." && return 1
     [ -d $DST ] && echo "Warning: directory \"$DST\" already exists! Skip..." && return 1
-    echo "Moving \"$SRC\" to \"$XWORK/\" ... Please, be patient!"
-    # mv $SRC $XWORK
-    cp -rf $SRC $XWORK && rm -rf $SRC
-    [ $? -eq 0 ] && echo "Error: move failure!" && return 1
+    if [ -d $SRC ] ; then
+        echo "Moving \"$SRC\" to \"$XWORK/\" ... Please, be patient!"
+        cp -rf $SRC $XWORK && rm -rf $SRC
+        [ $? -eq 0 ] && echo "Error: move failure!" && return 1
+    fi
     echo "Make link \"$SRC\" to \"$DST/\" ..."
     ln -s $DST -t $XHOME/
     [ $? -eq 0 ] && echo "Error: link failure!" && return 1
