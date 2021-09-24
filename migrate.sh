ec6c34ee3ed23ffe6baac35a51f5bf2f0ccf86ea
Aurelien Esnard <aurelien.esnard@u-bordeaux.fr>
2021-09-24 09:24:07 +0200

    replace mv with cp && rm

--- migrate.sh
+++ migrate.sh
@@ -7,10 +7,11 @@ function moveit() {
     local WHAT="$1"
     local SRC="$XHOME/$WHAT"
     local DST="$XWORK/$WHAT"
-    [ -L $SRC ] && echo "Warning: link \"$SRC\" already exists!" && return 1
-    [ -d $DST ] && echo "Warning: directory \"$DST\" already exists!" && return 1
+    [ -L $SRC ] && echo "Warning: link \"$SRC\" already exists! Skip..." && return 1
+    [ -d $DST ] && echo "Warning: directory \"$DST\" already exists! Skip..." && return 1
     echo "Moving \"$SRC\" to \"$XWORK/\" ... Please, be patient!"
-    mv $SRC $XWORK
+    # mv $SRC $XWORK
+    cp -rf $SRC $XWORK && rm -rf $SRC
     [ $? -eq 0 ] && echo "Error: move failure!" && return 1
     echo "Make link \"$SRC\" to \"$DST/\" ..."
     ln -s $DST -t $XHOME/
