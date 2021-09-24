8ba91f42090a1cf049fceef466faf7d704e6c44c
Aurelien Esnard <aurelien.esnard@u-bordeaux.fr>
2021-09-24 16:18:42 +0200

    improve migrate script

--- migrate.sh
+++ migrate.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-XHOME=/net/cremi/$USER
-XWORK=/net/travail/$USER
+WORK=/net/travail/$USER
 
 function moveit() {
-    local WHAT="$1"
-    local SRC="$XHOME/$WHAT"
-    local DST="$XWORK/$WHAT"
+    local SRC="$1"
+    local DST="$2"
+    local DIR=$(basename $SRC)
+    local TARGET="$DST/$DIR"
     [ -L $SRC ] && echo "Warning: link \"$SRC\" already exists! Skip..." && return 1
-    [ -d $DST ] && echo "Warning: directory \"$DST\" already exists! Skip..." && return 1
-    if [ -d $SRC ] ; then
-        echo "Moving \"$SRC\" to \"$XWORK/\" ... Please, be patient!"
-        cp -rf $SRC $XWORK && rm -rf $SRC
-        [ $? -eq 0 ] && echo "Error: move failure!" && return 1
+    [ -d $TARGET ] && echo "Warning: directory \"$TARGET\" already exists! Skip..." && return 1
+    [ -d $SRC ] ; then
+        echo "Moving \"$SRC\" to \"$DST/\"... Please, be patient!"
+        cp -rf $SRC $DST && rm -rf $SRC
+        [ $? -ne 0 ] && echo "Error: move failure!" && return 1
     fi
-    echo "Make link \"$SRC\" to \"$DST/\" ..."
+    echo "Make link \"$SRC\" to \"$TARGET/\" ..."
     local BASEDIRSRC=$(dirname $SRC)
-    ln -sf -t $BASEDIRSRC $DST
+    ln -sf -t $BASEDIRSRC $TARGET
     [ $? -eq 0 ] && echo "Error: link failure!" && return 1
     return 0
 }
 
-moveit .vscode
-moveit .config/Code
+moveit $HOME/.vscode $WORK
+mkdir -p $WORK/.config/
+moveit $HOME/.config/Code $WORK/.config/
