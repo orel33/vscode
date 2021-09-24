849619b1679815d79ba92baac09688d6d69c4861
Aurelien Esnard <aurelien.esnard@u-bordeaux.fr>
2021-09-24 16:54:31 +0200

    fix

--- reset.sh
+++ reset.sh
@@ -14,8 +14,8 @@ mkdir -p /net/travail/$USER/.vscode &> /dev/null
 mkdir -p /net/travail/$USER/.vscode-server &> /dev/null
 mkdir -p /net/travail/$USER/.config/Code &> /dev/null
 
-ln -sf /net/cremi/$USER/.vscode /net/travail/$USER/.vscode
-ln -sf /net/cremi/$USER/.vscode-server /net/travail/$USER/.vscode-server
-ln -sf /net/cremi/$USER/.config/Code /net/travail/$USER/.config/Code
+ln -sf /net/travail/$USER/.vscode /net/cremi/$USER/.vscode
+ln -sf /net/travail/$USER/.vscode-server /net/cremi/$USER/.vscode-server
+ln -sf /net/travail/$USER/.config/Code /net/cremi/$USER/.config/Code
 
 echo "Now, you can use VS Code safely at CREMI :-)"
