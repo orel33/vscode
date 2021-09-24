cfa418ba1e517e611cd2d4f2a3c3baadf59f3ee8
Aurelien Esnard <aurelien.esnard@u-bordeaux.fr>
2021-09-24 16:42:44 +0200

    reset all vscode config

---
+++ reset.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf /net/cremi/$USER/.vscode/
+rm -rf /net/cremi/$USER/.vscode-server/
+rm -rf /net/cremi/$USER/.config/Code/
+rm -rf /net/travail/$USER/.vscode/
+rm -rf /net/travail/$USER/.config/Code/
